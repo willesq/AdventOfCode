dddeb5bab50fad4e996497b68928e249dafcac70
William Esquivel <65838402+willesq@users.noreply.github.com>
2023-12-05 13:59:05 -0800

    finish day 5

--- cmd/2023/05/internal/day5.go
+++ cmd/2023/05/internal/day5.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 func Part1(filename string) (*Challenge, *int) {
 	derp := adventhelper.ReadFile(fmt.Sprintf(filename))
 	Input := Challenge{RawData: derp}
 	Input.init()
-	Input.findLocations()
+	Input.findLocations(Input.Seeds)
 	smallestLocation := 0
 
 	for _, val := range Input.Seeds {
@@ -27,14 +28,66 @@ func Part1(filename string) (*Challenge, *int) {
 	return &Input, &smallestLocation
 }
 
-func Part2(input *Challenge) *int {
-	var totalSum int
-	return &totalSum
+func Part2(Input *Challenge) *int {
+	Input.SeedsExpanded = []*Seed{}
+	seedSplit := strings.Split((*Input.RawData)[0], ": ")
+	seedPairs := []SeedPairs{}
+	tmpSeedPair := SeedPairs{}
+	for _, seed := range strings.Split(seedSplit[1], " ") {
+		if tmpSeedPair.Start == 0 {
+			tmpVal, _ := strconv.Atoi(seed)
+			tmpSeedPair.Start = tmpVal
+		} else if tmpSeedPair.Range == 0 {
+			tmpVal, _ := strconv.Atoi(seed)
+			tmpSeedPair.Range = tmpVal
+			seedPairs = append(seedPairs, tmpSeedPair)
+			tmpSeedPair = SeedPairs{}
+		}
+	}
+
+	var wg, wgResults sync.WaitGroup
+	lowestLocations := make(chan int)
+
+	for _, seedP := range seedPairs {
+		wg.Add(1)
+		go func(seedP SeedPairs) {
+			defer wg.Done()
+			lowestloc := -1
+			startSeedVal := seedP.Start
+			seedValRange := seedP.Range
+			for newSeedVal := startSeedVal; newSeedVal < startSeedVal+seedValRange; newSeedVal++ {
+				tmp_seed := Seed{Value: newSeedVal}
+				Input.evalSeed(&tmp_seed)
+				loc := tmp_seed.Location
+				if lowestloc == -1 {
+					lowestloc = loc
+				} else if loc < lowestloc {
+					lowestloc = loc
+				}
+			}
+			lowestLocations <- lowestloc
+		}(seedP)
+	}
+	lowest := -1
+	wgResults.Add(1)
+	go func() {
+		defer wgResults.Done()
+		for result := range lowestLocations {
+			if lowest == -1 || result < lowest {
+				lowest = result
+			}
+		}
+	}()
+	wg.Wait()
+	close(lowestLocations)
+	wgResults.Wait()
+	return &lowest
 }
 
 type Challenge struct {
 	RawData        *[]string
 	Seeds          []*Seed
+	SeedsExpanded  []*Seed
 	ConversionMaps map[string][]*SourceDestinationMap
 }
 
@@ -76,119 +129,133 @@ func (c *Challenge) init() {
 	}
 }
 
-func (c *Challenge) findLocations() {
-	for _, seed := range c.Seeds {
-		// Find Soil
-		found := false
-		for _, mapValue := range c.ConversionMaps["seed-to-soil"] {
-			sourceMin := mapValue.SourceRange
-			sourceMax := sourceMin + mapValue.RangeLength
-			if seed.Value >= sourceMin && seed.Value < sourceMax {
-				offset := seed.Value - sourceMin
-				seed.Soil = mapValue.DestinationRange + offset
-				found = true
-				break
-			}
-		}
-		if !found {
-			seed.Soil = seed.Value
-		}
+func (c *Challenge) findLocations(Seeds []*Seed) {
+	var wg sync.WaitGroup
+	for _, seed := range Seeds {
+		wg.Add(1)
+		//fmt.Print(idx, "\n")
+		seed := seed
+		go func() {
+			defer wg.Done()
+			s := seed
+			c.evalSeed(s)
+		}()
+	}
+	wg.Wait()
+	fmt.Print("Done\n")
+}
 
-		// Find fertilizer
-		found = false
-		for _, mapValue := range c.ConversionMaps["soil-to-fertilizer"] {
-			sourceMin := mapValue.SourceRange
-			sourceMax := sourceMin + mapValue.RangeLength
-			if seed.Soil >= sourceMin && seed.Soil < sourceMax {
-				offset := seed.Soil - sourceMin
-				seed.Fertilizer = mapValue.DestinationRange + offset
-				found = true
-				break
-			}
+func (c *Challenge) evalSeed(seed *Seed) {
+	// Find Soil
+	found := false
+	for _, mapValue := range c.ConversionMaps["seed-to-soil"] {
+		sourceMin := mapValue.SourceRange
+		sourceMax := sourceMin + mapValue.RangeLength
+		if (*seed).Value >= sourceMin && (*seed).Value < sourceMax {
+			offset := (*seed).Value - sourceMin
+			(*seed).Soil = mapValue.DestinationRange + offset
+			found = true
+			break
 		}
-		if !found {
-			seed.Fertilizer = seed.Soil
-		}
+	}
+	if !found {
+		(*seed).Soil = (*seed).Value
+	}
 
-		// Find water
-		found = false
-		for _, mapValue := range c.ConversionMaps["fertilizer-to-water"] {
-			sourceMin := mapValue.SourceRange
-			sourceMax := sourceMin + mapValue.RangeLength
-			if seed.Fertilizer >= sourceMin && seed.Fertilizer < sourceMax {
-				offset := seed.Fertilizer - sourceMin
-				seed.Water = mapValue.DestinationRange + offset
-				found = true
-				break
-			}
+	// Find fertilizer
+	found = false
+	for _, mapValue := range c.ConversionMaps["soil-to-fertilizer"] {
+		sourceMin := mapValue.SourceRange
+		sourceMax := sourceMin + mapValue.RangeLength
+		if (*seed).Soil >= sourceMin && (*seed).Soil < sourceMax {
+			offset := (*seed).Soil - sourceMin
+			(*seed).Fertilizer = mapValue.DestinationRange + offset
+			found = true
+			break
 		}
-		if !found {
-			seed.Water = seed.Fertilizer
-		}
+	}
+	if !found {
+		(*seed).Fertilizer = (*seed).Soil
+	}
 
-		// Find light
-		found = false
-		for _, mapValue := range c.ConversionMaps["water-to-light"] {
-			sourceMin := mapValue.SourceRange
-			sourceMax := sourceMin + mapValue.RangeLength
-			if seed.Water >= sourceMin && seed.Water < sourceMax {
-				offset := seed.Water - sourceMin
-				seed.Light = mapValue.DestinationRange + offset
-				found = true
-				break
-			}
+	// Find water
+	found = false
+	for _, mapValue := range c.ConversionMaps["fertilizer-to-water"] {
+		sourceMin := mapValue.SourceRange
+		sourceMax := sourceMin + mapValue.RangeLength
+		if (*seed).Fertilizer >= sourceMin && (*seed).Fertilizer < sourceMax {
+			offset := (*seed).Fertilizer - sourceMin
+			(*seed).Water = mapValue.DestinationRange + offset
+			found = true
+			break
 		}
-		if !found {
-			seed.Light = seed.Water
-		}
+	}
+	if !found {
+		(*seed).Water = (*seed).Fertilizer
+	}
 
-		// Find temperature
-		found = false
-		for _, mapValue := range c.ConversionMaps["light-to-temperature"] {
-			sourceMin := mapValue.SourceRange
-			sourceMax := sourceMin + mapValue.RangeLength
-			if seed.Light >= sourceMin && seed.Light < sourceMax {
-				offset := seed.Light - sourceMin
-				seed.Temp = mapValue.DestinationRange + offset
-				found = true
-				break
-			}
+	// Find light
+	found = false
+	for _, mapValue := range c.ConversionMaps["water-to-light"] {
+		sourceMin := mapValue.SourceRange
+		sourceMax := sourceMin + mapValue.RangeLength
+		if (*seed).Water >= sourceMin && (*seed).Water < sourceMax {
+			offset := (*seed).Water - sourceMin
+			(*seed).Light = mapValue.DestinationRange + offset
+			found = true
+			break
 		}
-		if !found {
-			seed.Temp = seed.Light
-		}
+	}
+	if !found {
+		(*seed).Light = (*seed).Water
+	}
 
-		// Find humidity
-		found = false
-		for _, mapValue := range c.ConversionMaps["temperature-to-humidity"] {
-			sourceMin := mapValue.SourceRange
-			sourceMax := sourceMin + mapValue.RangeLength
-			if seed.Temp >= sourceMin && seed.Temp < sourceMax {
-				offset := seed.Temp - sourceMin
-				seed.Humidity = mapValue.DestinationRange + offset
-				found = true
-				break
-			}
+	// Find temperature
+	found = false
+	for _, mapValue := range c.ConversionMaps["light-to-temperature"] {
+		sourceMin := mapValue.SourceRange
+		sourceMax := sourceMin + mapValue.RangeLength
+		if (*seed).Light >= sourceMin && (*seed).Light < sourceMax {
+			offset := (*seed).Light - sourceMin
+			(*seed).Temp = mapValue.DestinationRange + offset
+			found = true
+			break
 		}
-		if !found {
-			seed.Humidity = seed.Temp
-		}
+	}
+	if !found {
+		(*seed).Temp = (*seed).Light
+	}
 
-		// Find location
-		found = false
-		for _, mapValue := range c.ConversionMaps["humidity-to-location"] {
-			sourceMin := mapValue.SourceRange
-			sourceMax := sourceMin + mapValue.RangeLength
-			if seed.Humidity >= sourceMin && seed.Humidity < sourceMax {
-				offset := seed.Humidity - sourceMin
-				seed.Location = mapValue.DestinationRange + offset
-				found = true
-				break
-			}
+	// Find humidity
+	found = false
+	for _, mapValue := range c.ConversionMaps["temperature-to-humidity"] {
+		sourceMin := mapValue.SourceRange
+		sourceMax := sourceMin + mapValue.RangeLength
+		if (*seed).Temp >= sourceMin && (*seed).Temp < sourceMax {
+			offset := (*seed).Temp - sourceMin
+			(*seed).Humidity = mapValue.DestinationRange + offset
+			found = true
+			break
 		}
-		if !found {
-			seed.Location = seed.Humidity
+	}
+	if !found {
+		(*seed).Humidity = (*seed).Temp
+	}
+
+	// Find location
+	found = false
+	for _, mapValue := range c.ConversionMaps["humidity-to-location"] {
+		sourceMin := mapValue.SourceRange
+		sourceMax := sourceMin + mapValue.RangeLength
+		if (*seed).Humidity >= sourceMin && (*seed).Humidity < sourceMax {
+			offset := (*seed).Humidity - sourceMin
+			(*seed).Location = mapValue.DestinationRange + offset
+			found = true
+			break
 		}
+	}
+	if !found {
+		(*seed).Location = (*seed).Humidity
 	}
 }
 
@@ -201,6 +268,11 @@ type Seed struct {
 	Temp       int
 	Humidity   int
 	Location   int
+}
+
+type SeedPairs struct {
+	Start int
+	Range int
 }
 
 type SourceDestinationMap struct {
--- cmd/2023/05/main.go
+++ cmd/2023/05/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"adventOfCode/cmd/2023/05/internal"
 	"fmt"
+	"time"
 )
 
 func main() {
+	start := time.Now()
 	fpPrefix := "./cmd/2023/05"
 	inputFilename := "input.txt"
 
@@ -13,4 +15,6 @@ func main() {
 	fmt.Print("2023: Day 05 - Part 1: ", *sum1, "\n")
 	sum2 := internal.Part2(input)
 	fmt.Print("2023: Day 05 - Part 2: ", *sum2, "\n")
+
+	fmt.Printf("Total Time Taken: %v", time.Since(start))
 }
