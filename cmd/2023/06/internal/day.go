cd679e20a8b4840d268ee6010e0ea932cd798d9b
William Esquivel <65838402+willesq@users.noreply.github.com>
2023-12-05 21:28:56 -0800

    day 6 part 1, complete

--- cmd/2023/06/internal/day.go
+++ cmd/2023/06/internal/day.go
@@ -1,15 +1,16 @@
 package internal
 
-import (
-	"adventOfCode/internal/adventhelper"
-	"fmt"
-)
-
 func Part1(filename string) (*Challenge, *int) {
-	derp := adventhelper.ReadFile(fmt.Sprintf(filename))
-	Input := Challenge{RawData: derp}
-	derpInt := -1
-	return &Input, &derpInt
+	Input := Challenge{}
+	Input.init()
+
+	multiplied := 1
+	for _, r := range Input.Races {
+		r.getMinMaxMilliseconds()
+		multiplied *= r.TotalPossibleWays
+	}
+
+	return &Input, &multiplied
 }
 
 func Part2(Input *Challenge) *int {
@@ -18,5 +19,50 @@ func Part2(Input *Challenge) *int {
 }
 
 type Challenge struct {
-	RawData *[]string
+	Races []Race
+}
+
+func (c *Challenge) init() {
+	c.Races = []Race{
+		{Duration: 44, DistanceRecord: 208},
+		{Duration: 80, DistanceRecord: 1581},
+		{Duration: 65, DistanceRecord: 1050},
+		{Duration: 72, DistanceRecord: 1102},
+		//{Duration: 7, DistanceRecord: 9},
+		//{Duration: 15, DistanceRecord: 40},
+		//{Duration: 30, DistanceRecord: 200},
+	}
+}
+
+type Race struct {
+	Duration          int
+	DistanceRecord    int
+	MinSpeed          int
+	MaxSpeed          int
+	TotalPossibleWays int
+}
+
+func (r *Race) getMinMaxMilliseconds() {
+	r.MinSpeed = -1
+	r.MaxSpeed = -1
+	for i := 0; i <= r.Duration; i++ {
+		speed := i
+		remainingDur := r.Duration - i
+		distanceTRaveled := speed * remainingDur
+		if distanceTRaveled <= r.DistanceRecord {
+			// continue to next iteration if current doesn't beat record
+			continue
+		}
+		r.TotalPossibleWays += 1
+		if r.MinSpeed == -1 || speed < r.MinSpeed {
+			// update minimum speed
+			r.MinSpeed = speed
+		}
+
+		if r.MaxSpeed == -1 || speed > r.MaxSpeed {
+			// update maximum speed
+			r.MaxSpeed = speed
+		}
+	}
+	//r.TotalPossibleWays = r.MaxSpeed - r.MinSpeed
 }
